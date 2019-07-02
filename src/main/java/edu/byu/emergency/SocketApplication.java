9373675f9550d398bf0cae87374439f37673e073
Lauren Anderson <laurenra@byu.edu>
2019-07-02 14:01:08 -0600

    Configured as stand-alone server by default

--- src/main/java/edu/byu/emergency/SocketApplication.java
+++ src/main/java/edu/byu/emergency/SocketApplication.java
@@ -1,30 +1,14 @@
 package edu.byu.emergency;
 
-/* Use this to run as stand-alone Spring Boot server with "./gradlew bootRun */
-//@SpringBootApplication
-//public class SocketApplication {
-//
-//	public static void main(String[] args) {
-//		SpringApplication.run(SocketApplication.class, args);
-//	}
-//
-//}
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-/* Use this to run as a .war file on Tomcat in the webapps directory */
+/* Use this to run as stand-alone Spring Boot server with "./gradlew bootRun */
 @SpringBootApplication
-public class SocketApplication extends SpringBootServletInitializer {
+public class SocketApplication {
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder) {
-        return applicationBuilder.sources(SocketApplication.class);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(SocketApplication.class, args);
+	}
 
-    public static void main(String[] args) {
-        SpringApplication.run(SocketApplication.class, args);
-    }
 }
